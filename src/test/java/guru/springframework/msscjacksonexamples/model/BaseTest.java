83209ec66a411dcdd09a8833e688884fef2ab12a
Nemanja Tomanović <nemanja.tomanovic@gmail.com>
2020-07-01 09:20:36 +0200

    Adding custom json serializer

--- src/test/java/guru/springframework/msscjacksonexamples/model/BaseTest.java
+++ src/test/java/guru/springframework/msscjacksonexamples/model/BaseTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
@@ -11,7 +12,7 @@ public class BaseTest {
 
     @Autowired
     ObjectMapper objectMapper;
-    
+
     BeerDto getDto() {
         return BeerDto.builder()
                 .beerName("BeerName")
@@ -20,6 +21,7 @@ public class BaseTest {
                 .createdDate(OffsetDateTime.now())
                 .lastUpdatedDate(OffsetDateTime.now())
                 .price(new BigDecimal("12.99"))
+                .myLocalDate(LocalDate.now())
                 .upc(32131241242L)
                 .build();
     }
